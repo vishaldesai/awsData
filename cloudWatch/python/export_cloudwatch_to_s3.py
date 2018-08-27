59831a34ad18fb876492f1f19724e9b1b25faf81
vishaldesai <vishal.oracle@gmail.com>
2018-08-27 17:22:23 -0400

    Update export_cloudwatch_to_s3.py

--- cloudWatch/python/export_cloudwatch_to_s3.py
+++ cloudWatch/python/export_cloudwatch_to_s3.py
@@ -1,19 +1,20 @@
-###############################################################################################################################################################
-##### This script is used export cloudwatch logs into S3 bucket in batch mode                                                                               ###
-##### Only one export task can run at a time                                                                                                                ###
-##### https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/cloudwatch_limits_cwl.html                                                                   ###
-###############################################################################################################################################################
-##CHANGE LOG:                                                                                                                                               ###
-## Author     Date        Change Description                                                                                                                ###
-## #########  ##########  #####################################################################################################################################
-## Vishal     2018-08-25  Initial version.                                                                                                                  ###
-###############################################################################################################################################################
-## Prerequisite:                                                                                                                                            ###
-## 1. Create dynamodb table with batchid string (partition key), epoch_timestamp number (sort key), processed string                                        ###
-## 2. Add record in dynamodb table with partition key and timestamp from where you want to start copying logs from                                          ###
-###############################################################################################################################################################
-## Usage:python export_cloudwatch_to_s3.py --region us-east-1 --ddb cloudwatch6 --ddbstream cloudwatchtod0 --secondstoprocess 10800                         ###
-###############################################################################################################################################################
+#############################################################################################################################################################################
+##### This script is used export cloudwatch logs into S3 bucket in batch mode                                                                                             ###
+##### Only one export task can run at a time                                                                                                                              ###
+##### https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/cloudwatch_limits_cwl.html                                                                                 ###
+#############################################################################################################################################################################
+##CHANGE LOG:                                                                                                                                                             ###
+## Author     Date        Change Description                                                                                                                              ###
+## #########  ##########  ###################################################################################################################################################
+## Vishal     2018-08-25  Initial version.                                                                                                                                ###
+#############################################################################################################################################################################
+## Prerequisite:                                                                                                                                                          ###
+## 1. Create dynamodb table with batchid string (partition key), epoch_timestamp number (sort key), processed string                                                      ###
+## 2. Add record in dynamodb table with partition key and timestamp from where you want to start copying logs from. If you dont poplulate dyanmodb table                  ###
+##    it will process logs 1 hour before current timestamp                                                                                                                ###
+#############################################################################################################################################################################
+## Usage:python export_cloudwatch_to_s3.py --region us-east-1 --ddb cloudwatch6 --ddbstream cloudwatchtod0 --secondstoprocess 10800 --destinationbucket mytestbucket      ###
+#############################################################################################################################################################################
 
 
 import boto3
@@ -33,7 +34,10 @@ def query_table(table_name,stream,region):
         print(type(stream))
         response = dynamodb.query(TableName=table_name, KeyConditionExpression="batchid = :X",
                                   ExpressionAttributeValues={":X": {"S": stream}}, ScanIndexForward=False)
-        max_timestamp = response['Items'][response['Count']-1]['epoch_timestamp']['N']
+        if response['Count'] > 0:
+            max_timestamp = response['Items'][response['Count']-1]['epoch_timestamp']['N']
+        else:
+            max_timestamp = int(time.time()) - 3600
         return max_timestamp
     except ClientError as e:
         print(e.response)
@@ -63,6 +67,7 @@ def main(arguments):
     parser.add_argument('--ddb', required=True)
     parser.add_argument('--ddbstream', required=True)
     parser.add_argument('--secondstoprocess', required=True)
+    parser.add_argument('--destinationbucket',required=True)
     args = parser.parse_args(arguments)
 
     # Define client for log groups
@@ -77,20 +82,45 @@ def main(arguments):
     print("Current local timestamp : " + str(int(time.time())) + ' - ' + str(time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(int(time.time())))))
 
 
-    resultMessage = ""
-    logGroups = logsclient.describe_log_groups()
+
     if last_epoch_timestamp + int(args.secondstoprocess) < int(time.mktime(time.strptime(datetime.datetime.utcnow().strftime('%Y-%m-%d %H.%M.%S'), '%Y-%m-%d %H.%M.%S'))):
         start_epoch_timestamp = last_epoch_timestamp
         end_epoch_timestamp = last_epoch_timestamp + int(args.secondstoprocess)
         print('Processing start_epoch_timestamp in milliseconds: ' + str(last_epoch_timestamp*1000) + ' - ' + str(datetime.datetime.fromtimestamp(last_epoch_timestamp).strftime('%m/%d/%Y %H:%M:%S')))
         print('Processing end_epoch_timestamp in milliseconds : ' + str(end_epoch_timestamp*1000)  + ' - ' + str(datetime.datetime.fromtimestamp(end_epoch_timestamp).strftime('%m/%d/%Y %H:%M:%S')))
 
+
         try:
-            while "nextToken" in logGroups:
+            next_token = "initialize"
+            limit = 10
+            countLogGroups = 1
+            resultMessage = ""
+            
+            #while "nextToken" in logGroups:
+            while next_token:
+                
+                if str(next_token) != "None":
+                    prev_token = next_token
+                if next_token == "initialize":
+                    logGroups = logsclient.describe_log_groups(limit=limit)
+                else:
+                    logGroups = logsclient.describe_log_groups(
+                        nextToken=next_token, limit=limit)
+                
+                next_token = logGroups.get("nextToken")
+
+                if str(next_token) == "None" and limit != 1:
+                    next_token = prev_token
+                    limit = 1
+                    logGroups = logsclient.describe_log_groups(
+                        nextToken=next_token, limit=limit)
+                    next_token = logGroups.get("nextToken")
+
                 #Iterate through log groups
                 for logGroup in logGroups["logGroups"]:
-                    print("%s: %s" %
-                        (time.strftime('%Y-%d-%m %H:%M:%S'), logGroup["logGroupName"]))
+                    print("%s: %s: %s" %
+                        (time.strftime('%Y-%d-%m %H:%M:%S'), countLogGroups, logGroup["logGroupName"]))
+                    countLogGroups = countLogGroups + 1
                     resultMessage += "%s: %s\n" % (time.strftime('%Y-%d-%m %H:%M:%S'),
                                                 logGroup["logGroupName"])
                     exportTaskResponse = logsclient.create_export_task(
@@ -98,7 +128,7 @@ def main(arguments):
                         logGroupName=logGroup["logGroupName"],
                         fromTime=start_epoch_timestamp*1000,
                         to=end_epoch_timestamp*1000,
-                        destination='vishalintellidev',
+                        destination=args.destinationbucket,
                         destinationPrefix='ana%s/%s/%s/%s/%s/%s' % (logGroup["logGroupName"]                                        
                                             , time.strftime('%Y', time.localtime(end_epoch_timestamp))                                       
                                             , time.strftime('%m', time.localtime(end_epoch_timestamp))           
@@ -120,8 +150,7 @@ def main(arguments):
                         print(exportStatus["exportTasks"][0]["status"]["code"])
                         resultMessage += exportStatus["exportTasks"][0]["status"]["code"] + '\n'
 
-            logGroups = logsclient.describe_log_groups(
-                nextToken=logGroups["nextToken"])
+
         except ClientError as e:
             print(e.response)
 
